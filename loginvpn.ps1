daa6e9459b21ef8d7cdbe1f03f22dab3505481f9
symphony2014 <symphony2013@outlook.com>
2016-10-15 23:10:16 +0800

    first

---
+++ loginvpn.ps1
@@ -0,0 +1,11 @@
+﻿#$servers=Get-Content "D:\mycode\powershell\serviers_vpn.txt"
+#foreach($server in $servers){
+ #  $r=$server.Split(" ") 
+  # Add-VpnConnection -Name $r[0] -ServerAddress $r[1] -TunnelType Pptp -UseWinlogonCredential
+#}
+$names=Get-VpnConnection | select name
+$names
+$ran=Get-Random -Minimum 0 -Maximum ($names.Length-1)
+"test"
+$names[$ran].ToString()
+rasdial.exe $names[$ran].toString().Trim() symphony2015 01052193248
